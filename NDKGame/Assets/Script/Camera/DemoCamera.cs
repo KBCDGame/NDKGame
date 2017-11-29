a0cd89c241196d5bf752971291c79cf3505fbdd4
NoborioYoshiki <a>
2017-11-29 16:51:51 +0900

    プレイヤーPrefab追加

---
+++ NDKGame/Assets/Script/Camera/DemoCamera.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoCamera : MonoBehaviour {
+
+    //プレイヤー。
+    private GameObject Player = null;
+    //プレイヤーの位置。
+    private Vector3 PlayerPos = Vector3.zero;
+    //入力量保持用。
+    private float InputX, InputY = 0.0f;
+    //ホイールの入力量保持用。
+    private float Scroll = 0.0f;
+    //カメラが前後に移動したかどうかのフラグ。
+    private bool IsMoveBeforeOrAfterFlag = false;
+    ////カメラ。
+    //private Camera GemaCamera = null;
+    ////ズームに使う値保持用。
+    //private float Scroll, View = 0.0f;
+    [Header("回転速度。")]
+    public float RotateSpeed = 200.0f;
+    [Header("縦回転の上限。")]
+    [Space(10), Tooltip("今は設定しても効果無し。")]
+    public float MaxRotateY = 0.0f;
+    [Header("縦回転の下限。")]
+    [Space(10), Tooltip("今は設定しても効果無し。")]
+    public float MinRotateY = 0.0f;
+    [Header("プレイヤーからカメラまでの最大距離。")]
+    public float PlayerToCameraMaxDistance = 80.0f;
+    [Header("プレイヤーからカメラまでの最小距離。")]
+    public float PlayerToCameraMinDistance = 3.0f;
+    [Header("ゲームスタート時にプレイヤーからどれだけカメラをずらすか。")]
+    public Vector3 StartOffsetPos = Vector3.zero;
+    [Header("コントローラー使用時のカメラの前後移動のスピード。")]
+    public float ControllerCameraMoveBeforeAndAfterSpeed = 2.0f;
+    [Header("マウス使用時のカメラの前後移動のスピード。")]
+    public float MouseCameraMoveBeforeAndAfterSpeed = 1000.0f;
+    ////ズームの最大と最小。
+    //public float ZoomMax,ZoomMin = 0.0f;
+    ////ズームの時のスピード。
+    //public float ZoomSpeed = 1.0f;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //最大と最小が反転して設定されていた場合。
+        if (PlayerToCameraMinDistance > PlayerToCameraMaxDistance)
+        {
+            float work = PlayerToCameraMinDistance;
+            PlayerToCameraMinDistance = PlayerToCameraMaxDistance;
+            PlayerToCameraMaxDistance = work;
+            Debug.Log("プレイヤーとカメラ間の最小距離とプレイヤーとカメラ間の最大距離が逆でした。");
+
+        }
+        //プレイヤー取得。
+        Player = GameObject.FindWithTag("Player");
+        ////カメラ取得。
+        //GemaCamera = GetComponent<Camera>();
+        //プレイヤーの位置取得。
+        PlayerPos = Player.transform.position;
+        //カメラの位置をプレイヤーからずらした位置に設定。
+        Vector3 newPos = PlayerPos + StartOffsetPos;
+        newPos.z += PlayerToCameraMinDistance;
+        transform.position += newPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //プレイヤーの移動量分、自分(カメラ)も移動する
+        transform.position += Player.transform.position - PlayerPos;
+        PlayerPos = Player.transform.position;
+
+        //Input関係の処理。
+        InputRotate();
+
+        //PlayerPosの位置のY軸を中心に、回転（公転）する
+        transform.RotateAround(PlayerPos, Vector3.up, InputX);
+
+        //カメラの前後移動。
+        CameraMoveBeforeAndAfter();
+
+        //カメラの前後移動がされていないならカメラの回転をする。
+        if (IsMoveBeforeOrAfterFlag != true)
+        {
+            //カメラの垂直移動（角度制限なし）
+            transform.RotateAround(PlayerPos, transform.right, InputY);
+        }
+    }
+
+    //Inputの回転をまとまたもの。
+    private void InputRotate()
+    {
+        float DeltaSpeed = Time.deltaTime * RotateSpeed;
+        // マウスの右クリックを押している間
+        if (Input.GetMouseButton(1))
+        {
+            //マウスの移動量
+            InputX = Input.GetAxis("Mouse X") * DeltaSpeed;
+            InputY = Input.GetAxis("Mouse Y") * DeltaSpeed;
+        }
+        //マウスの右が押されていない時は右スティックの倒しを優先する。
+        else
+        {
+            //右スティックの移動量
+            InputX = Input.GetAxis("Horizontal2") * DeltaSpeed;
+            InputY = Input.GetAxis("Vertical2") * DeltaSpeed;
+        }
+
+        //ホイールの入力。
+        Scroll = Input.GetAxis("Mouse ScrollWheel");
+    }
+
+    ////カメラのズーム処理。
+    //private void CameraZoom()
+    //{
+    //    float view = GemaCamera.fieldOfView - Scroll * ZoomSpeed;
+    //    //Clampを使ってマイナスや極端に大きな値にならなよう調整。
+    //    GemaCamera.fieldOfView = Mathf.Clamp(value: view, min:ZoomMin, max: ZoomMax);
+    //}
+
+    //ホイールとパッドを使ったカメラの前後移動。
+    private void CameraMoveBeforeAndAfter()
+    {
+        //初期化。
+        float value = 0.0f;
+        float speed = 0.0f;
+
+        //移動フラグ初期化。
+        IsMoveBeforeOrAfterFlag = false;
+
+        //コントローラーが使われているならコントローラー用の前後移動のスピードを設定。
+        if (Input.GetKey(KeyCode.Joystick1Button4))
+        {
+            value = InputY;
+            speed = ControllerCameraMoveBeforeAndAfterSpeed;
+            IsMoveBeforeOrAfterFlag = true;
+        }
+
+        //マウスが使われているならマウス用の前後移動のスピードを設定。
+        if (Scroll != 0.0f)
+        {
+            value = Scroll;
+            speed = MouseCameraMoveBeforeAndAfterSpeed;
+            IsMoveBeforeOrAfterFlag = true;
+        }
+
+        //どれくらい移動するか。
+        Vector3 MovePos = transform.forward * -value * Time.deltaTime * speed;
+
+        //カメラとプレイヤーの距離計算。
+        float distance = (transform.position.z + MovePos.z) - PlayerPos.z;
+
+        //スティックが前後どちらかに倒されていて、プレイヤーとカメラの距離が指定範囲内なら移動。
+        if ((value > 0.0f && Mathf.Abs(distance) < PlayerToCameraMaxDistance) || (value < 0.0f && Mathf.Abs(distance) > PlayerToCameraMinDistance))
+        {
+            transform.position += MovePos;
+        }
+    }
+}
---
+++ NDKGame/Assets/Script/Player/DemoPlayer.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoPlayer : MonoBehaviour {
+
+    //入力された値保持用。
+    private float InputHorizontal = 0.0f;
+    private float InputVertical = 0.0f;
+
+    [Header("移動速度。")]
+    public float MoveSpeed = 0.0f;
+    [Header("ジャンプ速度。")]
+    public float JumpSpeed = 0.0f;
+    [Header("重力。")]
+    public float Gravity = 0.0f;
+    //カメラ。
+    private GameObject Camera = null;
+    //キャラクターコントローラー。
+    private CharacterController CharaCon = null;
+    //移動方向。
+    private Vector3 MoveDirection = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        //キャラクターコントローラー取得。
+        CharaCon = GetComponent<CharacterController>();
+        //カメラ取得。
+        Camera = GameObject.Find("DemoCamera");
+    }
+
+    //Inputの入力漏れを無くすためこっちに記述。
+    void Update()
+    {
+        //入力値を格納。
+        InputHorizontal = Input.GetAxis("Horizontal");
+        InputVertical = Input.GetAxis("Vertical");
+
+        //接地判定。
+        if (CharaCon.isGrounded)
+        {
+            //接地中はジャンプ可。
+            if (Input.GetButton("Jump"))
+            {
+                //ジャンプ。
+                Jump();
+            }
+        }
+    }
+
+    //移動を安定させるためにFixedUpdateにした。
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // カメラの方向から、X-Z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(Camera.transform.forward, new Vector3(1.0f, 0.0f, 1.0f)).normalized;
+
+        // 方向キーの入力値とカメラの向きから、移動方向を決定
+        Vector3 moveForward = cameraForward * InputVertical + Camera.transform.right * InputHorizontal;
+
+        // 移動方向にスピードを掛ける。
+        MoveDirection.x = moveForward.x * MoveSpeed;
+        MoveDirection.z = moveForward.z * MoveSpeed;
+
+        // キャラクターの向きを進行方向に
+        if (moveForward != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveForward);
+        }
+        //重力計算。
+        MoveDirection.y -= Gravity * Time.fixedDeltaTime;
+        //計算した移動量をキャラコンに渡す。
+        CharaCon.Move(MoveDirection * Time.fixedDeltaTime);
+    }
+
+    //ジャンプの処理。
+    void Jump()
+    {
+        MoveDirection.y = JumpSpeed;
+    }
+
+    //if (Input.GetKey(KeyCode.Joystick1Button0))
+    //{
+    //    Debug.Log("Button A Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button1))
+    //{
+    //    Debug.Log("Button B Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button2))
+    //{
+    //    Debug.Log("Button X Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button3))
+    //{
+    //    Debug.Log("Button Y Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button4))
+    //{
+    //    Debug.Log("Button LB Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button5))
+    //{
+    //    Debug.Log("Button RB Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button6))
+    //{
+    //    Debug.Log("Button Back Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button7))
+    //{
+    //    Debug.Log("Button START Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button8))
+    //{
+    //    Debug.Log("L Stick Push Push");
+    //}
+    //if (Input.GetKey(KeyCode.Joystick1Button9))
+    //{
+    //    Debug.Log("R Stick Push");
+    //}
+    //float TrigerInput = Input.GetAxis("Triger");
+    //if (TrigerInput < 0.0f)
+    //{
+    //    Debug.Log("L Triger");
+    //}
+    //else if (TrigerInput > 0.0f)
+    //{
+    //    Debug.Log("R Triger");
+    //}
+    //float HorizontalKeyInput = Input.GetAxis("HorizontalKey");
+    //if (HorizontalKeyInput < 0.0f)
+    //{
+    //    Debug.Log("Left Key");
+    //}
+    //else if (HorizontalKeyInput > 0.0f)
+    //{
+    //    Debug.Log("Right Key");
+    //}
+    //float VerticalKeyInput = Input.GetAxis("VerticalKey");
+    //if (VerticalKeyInput < 0.0f)
+    //{
+    //    Debug.Log("Up Key");
+    //}
+    //else if (VerticalKeyInput > 0.0f)
+    //{
+    //    Debug.Log("Down Key");
+    //}	
+}
